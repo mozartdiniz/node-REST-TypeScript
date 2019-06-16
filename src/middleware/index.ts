241a294b61190cb3869dd1a6d15b60ef2f663259
Mozart Diniz <mozart.diniz@gmail.com>
2019-06-16 12:10:55 +0100

    docs with sagger and custom error handler

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,4 +4,14 @@ import {
     handleCompression
 } from './common';
 
-export default [handleCors, handleBodyRequestParsing, handleCompression];
+import {
+    handleAPIDocs
+} from './apiDocs';
+
+export default [
+    handleCors,
+    handleBodyRequestParsing,
+    handleCompression,
+    handleAPIDocs
+];
+
